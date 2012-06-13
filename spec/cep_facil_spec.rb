87a7f8903b644b65befd824743bd7d7366679273
Rodrigo Vieira <rodrigovieira1994@gmail.com>
2012-06-13 15:38:45 -0300

    Cleaning up specs.

--- spec/cep_facil_spec.rb
+++ spec/cep_facil_spec.rb
@@ -5,51 +5,51 @@ describe CepFacil do
   before(:each) do
     @token = "0E2ACA03-FC7F-4E87-9046-A8C46637BA9D"
   end
- 
+
   it "has a version" do
     CepFacil::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
-  
+
   it "can fetch an address from a CEP" do
     cep = "53417-540"
-    
+
     address = CepFacil.get_address cep, @token
     address.should_not be_nil
 
     address[:type].should eql("Rua")
-    address[:city].should eql("Paulista")    
+    address[:city].should eql("Paulista")
   end
-  
+
   it "works with non formatted zip codes (CEPs)" do
     cep = "53417540"
     address = CepFacil.get_address cep, @token
     address.should_not be_nil
   end
-  
+
   it "works even with zip codes (CEPs) stored as integers" do
     cep = 53417540
     address = CepFacil.get_address cep, @token
     address.should_not be_nil
   end
-  
+
   it "can get all 6 attributes (cep, type, state, city, neighborhood, description) from a given CEP" do
     cep = "52060-010"
     address = CepFacil.get_address cep, @token
-    
+
     address[:cep].should eql("52060010")
     address[:type].should eql("Rua")
     address[:state].should eql("PE")
     address[:city].should eql("Recife")
     address[:neighborhood].should eql("Parnamirim")
     address[:description].should include("Tude de Melo")
-    
+
     address[:cep].should_not be_nil
     address[:type].should_not be_nil
     address[:state].should_not be_nil
     address[:city].should_not be_nil
     address[:neighborhood].should_not be_nil
     address[:description].should_not be_nil
-    
+
     address.should have_key(:cep)
     address.should have_key(:type)
     address.should have_key(:state)
@@ -57,7 +57,7 @@ describe CepFacil do
     address.should have_key(:neighborhood)
     address.should have_key(:description)
   end
-  
+
   it "can return a string with the full address" do
     cep = "53417-540"
     address = CepFacil.get_address cep, @token
@@ -88,22 +88,6 @@ describe CepFacil do
     address[:uf].should eql("MS")
     address[:district].should eql("Amambaí")
   end
-=begin  
-  it "has a working alternative usage" do
-    include CepFacil
-    a = RSpec::Core::ExampleGroup.new
-    a.extend(CepFacil)
-    
-    cep = "50050-000"
-    address = get_address cep, @token
-    full_version = full address
-    
-    address.should_not be_nil
-    full_version.should_not be_nil
-    
-    full_version.should eql("Rua da Aurora, Boa Vista, Recife-PE, Brasil.")
-  end
-=end
 
 end
 
