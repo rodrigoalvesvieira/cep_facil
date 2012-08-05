ac5b7aa8cb239e8c66b53927b93eb648af4f2d04
Rodrigo Alves Vieira <rodrigovieira1994@gmail.com>
2012-08-05 08:44:36 -0300

    Fixing 6 of 7 specs

--- spec/cep_facil_spec.rb
+++ spec/cep_facil_spec.rb
@@ -13,7 +13,7 @@ describe CepFacil do
   it "can fetch an address from a CEP" do
     cep = "53417-540"
 
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
     address.should_not be_nil
 
     address.type.should eql("Rua")
@@ -22,32 +22,32 @@ describe CepFacil do
 
   it "works with non formatted zip codes (CEPs)" do
     cep = "53417540"
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
     address.should_not be_nil
   end
 
   it "works even with zip codes (CEPs) stored as integers" do
     cep = 53417540
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
     address.should_not be_nil
   end
 
-  it "can get all 6 attributes (cep, type, state, city, neighborhood, description) from a given CEP" do
+  it "can get all 6 attributes (cep, type, state, city, neighborhood, street) from a given CEP" do
     cep = "52060-010"
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
 
     address.cep.should eql("52060010")
     address.type.should eql("Rua")
     address.state.should eql("PE")
     address.city.should eql("Recife")
     address.neighborhood.should eql("Parnamirim")
-    address.description.should include("Tude de Melo")
+    address.street.should include("Tude de Melo")
   end
 
   it "can return a string with the full address" do
     cep = "53417-540"
 
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
 
     full_version = address.full_format
 
@@ -58,10 +58,10 @@ describe CepFacil do
   it "handles correct encoding" do
     cep = "79005-340"
 
-    address = CepFacil::API.new(cep, token)
+    address = CepFacil::API.new(cep, @token)
 
     address.neighborhood.should eql("Amambaí")
-    address.description.should eql("Coronel Camisão")
+    address.street.should eql("Coronel Camisão")
   end
 
 end
