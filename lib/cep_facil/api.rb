d904c88ac5b393703a352578357f2671b86a2f96
Rodrigo Alves Vieira <rodrigovieira1994@gmail.com>
2012-11-05 12:05:54 -0300

    Improving code style

--- lib/cep_facil/api.rb
+++ lib/cep_facil/api.rb
@@ -6,11 +6,19 @@ module CepFacil
     attr_reader :read
 
     BASE_URL = "http://www.cepfacil.com.br/service/?filiacao=%s&cep=%s&formato=%s"
-    METHODS_PLACEHOLDER = {estado: :uf , type: :tipo , state: :uf , city: :cidade , neighborhood: :bairro , street: :descricao , rua: :descricao}
+    PLACEHOLDER_METHODS = {
+      estado: :uf,
+      type: :tipo,
+      state: :uf,
+      city: :cidade,
+      neighborhood: :bairro,
+      street: :descricao,
+      rua: :descricao
+    }
 
     class << self
-      ##
-      # Removes all non-numeric caracters and limit to 8 caracters
+      # Removes all non-numeric caracters from the zip_code argument
+      # and limit it to 8 caracters
       def parse_zip_code(zip_code)
         return zip_code.to_s.gsub(/([^0-9]+)/,"")[0...8]
       end
@@ -29,20 +37,17 @@ module CepFacil
       return self
     end
 
-    ##
     # Returns true if the address in question is a valid one
     # returns false otherwise
     def valid?
       @data.is_a?(Hash) and @data.size > 1
     end
 
-    #
     # Returns the address in its extense format
     def full_format
       "#{type} #{street}, #{city} - #{state}, Brasil"
     end
 
-    #
     # Returns true if the address was found with the given zip code (CEP)
     # returns false otherwise
     def found?
@@ -54,11 +59,10 @@ module CepFacil
     end
 
     private
-    #
     # Define placeholder methods for the address attributes
-    # in portuguese
+    # in pt-BR
     def method_missing(*args)
-      return (@data[METHODS_PLACEHOLDER[args[0].to_sym].to_s] || @data[args[0].to_s]).force_encoding(Encoding::UTF_8) || super(*args)
+      return (@data[PLACEHOLDER_METHODS[args[0].to_sym].to_s] || @data[args[0].to_s]).force_encoding(Encoding::UTF_8) || super(*args)
     end
 
     alias :address :full_format
